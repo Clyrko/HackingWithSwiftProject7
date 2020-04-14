c9652755d913e71c8c280c85e42421c65abe6983
Jay A <justynadusei@gmail.com>
2020-04-14 10:39:39 +0000

    Initial Commit

---
+++ Swift100Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift100Project7
+//
+//  Created by Jay A. on 4/14/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
