d7ce57b5fff76a8db9f0454b75266742a897cbe3
Jay A <justynadusei@gmail.com>
2020-04-14 12:20:04 +0000

    Added new struct

---
+++ Swift100Project7/Petitions.swift
@@ -0,0 +1,15 @@
+//
+//  Petitions.swift
+//  Swift100Project7
+//
+//  Created by Jay A. on 4/14/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    
+    var results: [Petition]
+    
+}
