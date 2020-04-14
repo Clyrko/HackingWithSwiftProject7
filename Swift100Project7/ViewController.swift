cee677162abec33ad01b396c29f3b149a174cd6e
Jay A <justynadusei@gmail.com>
2020-04-14 12:41:14 +0000

    Set URL to pull JSON data

--- Swift100Project7/ViewController.swift
+++ Swift100Project7/ViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var petitions = [String]()
+    var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                
+                parse(json: data)
+                
+            }
+        }
+    }
+    
+    func parse(json: Data) {
         
-        
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+            
+        }
     }
     
     //Setting row number equal to number of items in array petitions
@@ -28,8 +47,10 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        let petition = petitions[indexPath.row]
+        
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         
         return cell
         
