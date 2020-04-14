15163e7901138c7674beb26ea299620bd0a52209
Jay A <justynadusei@gmail.com>
2020-04-14 11:47:51 +0000

    Created struct that can parse JSON

---
+++ Swift100Project7/Petition.swift
@@ -0,0 +1,17 @@
+//
+//  Petition.swift
+//  Swift100Project7
+//
+//  Created by Jay A. on 4/14/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    
+    var title: String
+    var body: String
+    var signatureCount: Int
+    
+}
