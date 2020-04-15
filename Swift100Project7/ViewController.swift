b8ae68b0241a98adb7ce05a4db74c5c5aaf8986b
Jay A <justynadusei@gmail.com>
2020-04-15 09:56:39 +0000

    Error Handling

--- Swift100Project7/ViewController.swift
+++ Swift100Project7/ViewController.swift
@@ -15,14 +15,16 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
 
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                
-                parse(json: data)
-                
-            }
+        if navigationController?.tabBarItem.tag == 0 {
+            
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+            
+        } else {
+            
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+            
         }
     }
     
@@ -35,6 +37,14 @@ class ViewController: UITableViewController {
             tableView.reloadData()
             
         }
+    }
+    
+    func showError() {
+        
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
     }
     
     //Setting row number equal to number of items in array petitions
@@ -56,5 +66,10 @@ class ViewController: UITableViewController {
         
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
